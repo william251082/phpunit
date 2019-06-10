0a2f32ecb9954671d2a36a7b2b2e5063705943c3
William del Rosario <williamdelrosario@yahoo.com>
2019-06-10 12:16:33 +0200

    Secure the Park - installed phpunit

---
+++ tests/AppBundle/Entity/DinosaurTest.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Tests\AppBundle\Entity;
+
+use PHPUnit\Framework\TestCase;
+
+class DinosaurTest extends TestCase
+{
+    public function testThatYourComputerWorks()
+    {
+        $this->assertTrue(false);
+    }
+}
