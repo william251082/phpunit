95ff4c6d86748fe9b4c863ac72908b3cadb1c6d2
William del Rosario <williamdelrosario@yahoo.com>
2019-06-10 13:40:25 +0200

    Hooks: setup()

--- tests/AppBundle/Factory/DinosaurFactoryTest.php
+++ tests/AppBundle/Factory/DinosaurFactoryTest.php
@@ -8,10 +8,19 @@ use PHPUnit\Framework\TestCase;
 
 class DinosaurFactoryTest extends TestCase
 {
+    /**
+     * @var DinosaurFactory
+     */
+    private $factory;
+
+    public function setUp()
+    {
+        $this->factory = new DinosaurFactory();
+    }
+
     public function testItGrowsAVelociraptor()
     {
-        $factory = new DinosaurFactory();
-        $dinosaur = $factory->growVelociraptor(5);
+        $dinosaur = $this->factory->growVelociraptor(5);
 
         $this->assertInstanceOf(Dinosaur::class, $dinosaur);
         $this->assertInternalType('string', $dinosaur->getGenus());
