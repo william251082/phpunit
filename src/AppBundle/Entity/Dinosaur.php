4beae0b1a9ca6dfabf80cacb15e31af596ed1842
William del Rosario <williamdelrosario@yahoo.com>
2019-06-10 12:33:12 +0200

    Test dinosaur length - Tests, assertions, coding

---
+++ src/AppBundle/Entity/Dinosaur.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace AppBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity
+ * @ORM\Table(name="dinosaurs")
+ */
+class Dinosaur
+{
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private $length = 0;
+
+    /**
+     * @return mixed
+     */
+    public function getLength(): int
+    {
+        return $this->length;
+    }
+
+    /**
+     * @param mixed $length
+     */
+    public function setLength($length)
+    {
+        $this->length = $length;
+    }
+}
--- tests/AppBundle/Entity/DinosaurTest.php
+++ tests/AppBundle/Entity/DinosaurTest.php
@@ -2,12 +2,26 @@
 
 namespace Tests\AppBundle\Entity;
 
+use AppBundle\Entity\Dinosaur;
 use PHPUnit\Framework\TestCase;
 
 class DinosaurTest extends TestCase
 {
-    public function testThatYourComputerWorks()
+    public function testSettingLength()
     {
-        $this->assertTrue(false);
+        $dinosaur = new Dinosaur();
+
+        $this->assertSame(0, $dinosaur->getLength());
+
+        $dinosaur->setLength(9);
+        $this->assertSame(9, $dinosaur->getLength());
+    }
+
+    public function testDinosaurHasNotShrunk()
+    {
+        $dinosaur = new Dinosaur();
+        $dinosaur->setLength(15);
+
+        $this->assertGreaterThan(16, $dinosaur->getLength(), 'Why did this dinosaur shrunk?');
     }
 }
