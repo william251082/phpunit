a568aa947746c6f937f169b21a134bcd53e54433
William del Rosario <williamdelrosario@yahoo.com>
2019-06-21 15:11:54 +0200

    Partial Mocking

--- tests/AppBundle/Service/EnclosureBuilderServiceIntegrationTest.php
+++ tests/AppBundle/Service/EnclosureBuilderServiceIntegrationTest.php
@@ -5,9 +5,11 @@ namespace Tests\AppBundle\Service;
 use AppBundle\Entity\Dinosaur;
 use AppBundle\Entity\Enclosure;
 use AppBundle\Entity\Security;
+use AppBundle\Factory\DinosaurFactory;
 use AppBundle\Service\EnclosureBuilderService;
 use Doctrine\Common\DataFixtures\Purger\ORMPurger;
 use Doctrine\ORM\EntityManager;
+use function foo\func;
 use Symfony\Bundle\FrameworkBundle\Test\KernelTestCase;
 
 class EnclosureBuilderServiceIntegrationTest extends KernelTestCase
@@ -22,8 +24,21 @@ class EnclosureBuilderServiceIntegrationTest extends KernelTestCase
     public function testItBuildsEnclosureWithDefaultSpecification()
     {
         /** @var EnclosureBuilderService $enclosureBuilderService */
-        $enclosureBuilderService = self::$kernel->getContainer()
-            ->get('test.'.EnclosureBuilderService::class);
+//        $enclosureBuilderService = self::$kernel->getContainer()
+//            ->get('test.'.EnclosureBuilderService::class);
+
+        $dinoFactory = $this->createMock(DinosaurFactory::class);
+        $dinoFactory->expects($this->any())
+            ->method('growFromSpecification')
+            ->willReturnCallback(function($spec) {
+                return new Dinosaur();
+            });
+
+        $enclosureBuilderService = new EnclosureBuilderService(
+            $this->getEntityManager(),
+            $dinoFactory
+        );
+
 
         $enclosureBuilderService->buildEnclosure();
 
